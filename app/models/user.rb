070629f56b149c49587b23ab037500be54213280
tomokazu-sema <t.sema.1986@gmail.com>
2022-03-03 12:07:42 +0900

    バリデーションの設定(カリキュラムに合わせる)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :name, uniqueness: true
 end
